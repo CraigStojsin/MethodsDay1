362dc02cfebca62bf31e861eeabf3114acc4a457
CraigStojsin <stojsin@outlook.com>
2015-04-10 14:04:40 -0400

    Initial Commit

---
+++ MethodsDay1/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  MethodsDay1
+//
+//  Created by Craig Stojsin on 2015-04-10.
+//  Copyright (c) 2015 Codes By Craig. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
